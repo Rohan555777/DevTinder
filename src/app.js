e936473967ffde87d8720d15705bcecde7f1f78f
Rohan Kharat <kharatrohan555@gmail.com>
2026-06-30 17:57:58 +0530

    explore advance routing req.params

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
 const express = require("express");
 
 const app = express();
-//it will just handle get calls of user 
-app.get('/hello',(req, res) => {
+//it will just handle get calls of user
+app.get('/user/:userIdb', (req, res) => {
+  console.log(req.params)
   res.send("Hello hello from the server! rohan (get)");
 });
-app.post('/hello',(req, res) => {
-  res.send("Hello hello from the server! rohan (post)");
-});
-app.delete('/user',(req, res) => {
-  res.send("deleted successfully");
-});
-
-//it will handle all calls of user
-app.use('/hello/4',(req, res) => {
-  res.send("Hello from the server! rohan nana");
-});
-
-
 
 app.listen(1000, () => {
   console.log("rohan is watching");
 });
-  
 
-// it will check routing from top to bottom 
+/*
+
+it will check routing from top to bottom
+
+
+ /.ab?c/ :  this ? Because the letter b is optional, this single route will match two different URLs:
+
+ /.ab+c/  : The + means the b can repeat. Matches /abc, /abbc, /abbbc.
+
+The * is a wildcard. Matches /abcd, /abRANDOMcd, /ab123cd.
+
+"/ab(cd)?e", ...) thsi is old way to use this in '' now you use regex
+
+The () groups characters. The cd block is optional. Matches /abe and /abcde.
+
+*/
