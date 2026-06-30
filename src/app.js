0fe4b02855e688224d918c9d1fab78c02162a1d2
Rohan Kharat <kharatrohan555@gmail.com>
2026-06-30 17:28:39 +0530

    explore api request methods GET ,POST , DELETE and PUT

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 
 const app = express();
-app.use('/hello',(req, res) => {
-  res.send("Hello hello from the server! rohan");
+//it will just handle get calls of user 
+app.get('/hello',(req, res) => {
+  res.send("Hello hello from the server! rohan (get)");
 });
-app.use('/test',(req, res) => {
-  res.send("Hello from the server! rohan");
+app.post('/hello',(req, res) => {
+  res.send("Hello hello from the server! rohan (post)");
 });
+app.delete('/user',(req, res) => {
+  res.send("deleted successfully");
+});
+
+//it will handle all calls of user
+app.use('/hello/4',(req, res) => {
+  res.send("Hello from the server! rohan nana");
+});
+
+
+
 app.listen(1000, () => {
   console.log("rohan is watching");
 });
   
+
+// it will check routing from top to bottom 
